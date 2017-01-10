9c1062f0ba3cde69d23713ebb3efc9dfcf280930
Alex <dovzhanyn.alex@gmail.com>
2017-01-09 19:23:30 -0500

    trying compression again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.middleware.insert_before(Rack::Sendfile, Rack::Deflater)
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -94,4 +94,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.middleware.insert_before(Rack::Sendfile, Rack::Deflater)
 end
