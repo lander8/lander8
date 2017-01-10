77c6e8132d87c928efdd3d83c59ebd59e895a5f4
Alex <dovzhanyn.alex@gmail.com>
2017-01-09 22:52:48 -0500

    refactors contact success code

--- app/assets/javascripts/modals.js
+++ app/assets/javascripts/modals.js
@@ -13,25 +13,7 @@ $(document).ready(function(){
 			success: function(data)
 			{
 				$('#bugReportModal').modal('hide');
-				$('body').append(
-					"<div class='modal fade' id='successModal' tabindex='-1' role='dialog' aria-labelledby='myModalLabel'>" +
-					  "<div class='modal-dialog' role='document'>" +
-					    "<div class='modal-content'>" +
-					      "<div class='modal-header'>" +
-					        "<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
-					        "<h4 class='modal-title' id='myModalLabel'>Success!</h4>" +
-					      "</div>" +
-					      "<div class='modal-body'>" +
-					        "<p>Your report was submitted. Thanks for helping make Lander8 better!</p>" +
-					      "</div>" +
-					      "<div class='modal-footer'>" +
-					      	"<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>" +
-					      "</div>" +
-					    "</div>" +
-					  "</div>" +
-					"</div>"
-				);
-				$('#successModal').modal('show');
+				successModal("Success!", "Your report was submitted. Thanks for helping make Lander8 better!")	
 			}
   	});
   })
@@ -50,26 +32,30 @@ $(document).ready(function(){
 			success: function(data)
 			{
 				$('#contactModal').modal('hide');
-				$('body').append(
-					"<div class='modal fade' id='successModal' tabindex='-1' role='dialog' aria-labelledby='myModalLabel'>" +
-					  "<div class='modal-dialog' role='document'>" +
-					    "<div class='modal-content'>" +
-					      "<div class='modal-header'>" +
-					        "<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
-					        "<h4 class='modal-title' id='myModalLabel'>Success!</h4>" +
-					      "</div>" +
-					      "<div class='modal-body'>" +
-					        "<p>Your email was sent. Please allow up to 5 business days for a reply.</p>" +
-					      "</div>" +
-					      "<div class='modal-footer'>" +
-					      	"<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>" +
-					      "</div>" +
-					    "</div>" +
-					  "</div>" +
-					"</div>"
-				);
-				$('#successModal').modal('show');
+				successModal("Success!", "Your email was sent. Please allow up to 5 business days for a reply.");				
 			}
   	});
   })
 })
+
+function successModal(header, body) {
+	$('body').append(
+		"<div class='modal fade' id='successModal' tabindex='-1' role='dialog' aria-labelledby='myModalLabel'>" +
+		  "<div class='modal-dialog' role='document'>" +
+		    "<div class='modal-content'>" +
+		      "<div class='modal-header'>" +
+		        "<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
+		        "<h4 class='modal-title' id='myModalLabel'>" + header + "</h4>" +
+		      "</div>" +
+		      "<div class='modal-body'>" +
+		        "<p>" + body + "</p>" +
+		      "</div>" +
+		      "<div class='modal-footer'>" +
+		      	"<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>" +
+		      "</div>" +
+		    "</div>" +
+		  "</div>" +
+		"</div>"
+	);
+	$('#successModal').modal('show');
+}
