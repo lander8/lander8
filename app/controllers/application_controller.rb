5d16b934a65dec5144e8ae5c80d38794fa4b43b9
Alex <dovzhanyn.alex@gmail.com>
2017-01-12 00:04:43 -0500

    fix mobile raven fail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
 
 	# This is what gets sent to Sentry on errors
 	def set_raven_context
-		Raven.user_context(id: current_user.id, email: current_user.email, ip_address: request.ip)
+		Raven.user_context(id: current_user.id || nil, email: current_user.email || nil, ip_address: request.ip)
 		Raven.extra_context(params: params.to_unsafe_h, url: request.url)
 	end
 end
